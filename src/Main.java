d5cf500ffc976c7618ce50d82936d63b51bf6093
namilan <nichole.milan@gmail.com>
2020-04-27 14:56:49 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ public class Main {
 	
 	public static void main (String[] args) {
 		
+		int result = 1 + 2; // 1 + 2 = 3
+		System.out.println("1 + 2 = " + result);
+		
+		int previousResult = result;
+		System.out.println("previousResult = " + previousResult);
+		
+		result = result - 1;  // 3 - 1 = 2
+		System.out.println("3 - 1 = " + result);
+		System.out.println("previousResult = " + previousResult);
+		
 	}
 
 }
