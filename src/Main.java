a1f87eaf24480ae4c68fb46e3066cf9ac7d3ffca
namilan <nichole.milan@gmail.com>
2020-05-20 15:42:17 -0400

    ternary operator added.

--- src/Main.java
+++ src/Main.java
@@ -74,6 +74,13 @@ public class Main {
 		if(!isCar) {
 			System.out.println("This is not supposed to happen.");
 		}
+		
+		isCar = true;
+		boolean wasCar = isCar ? true : false; 
+		if(wasCar) {
+			System.out.println("wasCar is true");
+		}
 	}
+	
 
 }
