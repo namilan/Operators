07e68fee2e831f65f71894d42f43e6e7608d3f48
namilan <nichole.milan@gmail.com>
2020-05-20 15:24:54 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -51,11 +51,15 @@ public class Main {
 				System.out.println("And I am scared of aliens");
 		}
 		
-		int topScore = 100;
-		if (topScore == 100) {
+		int topScore = 80;
+		if (topScore < 100) {
 			System.out.println("You got the high score!");
 		}
 	
+		int secondTopScore = 60;
+		if (topScore > secondTopScore && topScore < 100) {
+			System.out.println("Greater than second top score and less than 100.");
+		}
 		
 	}
 
