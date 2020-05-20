b1e2ca426b5c271d0d4b83668e444571df660cb8
namilan <nichole.milan@gmail.com>
2020-05-20 15:20:15 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -50,6 +50,11 @@ public class Main {
 				System.out.println("It is not an alien!");
 				System.out.println("And I am scared of aliens");
 		}
+		
+		int topScore = 100;
+		if (topScore == 100) {
+			System.out.println("You got the high score!");
+		}
 	
 		
 	}
